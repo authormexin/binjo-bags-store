5b307005412bbda983f47fd0123215ecdf484cd4
authormexin <authormexin@gmail.com>
2026-04-16 10:27:42 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,26 +1,14 @@
-const products = [
+window.products = [
   {
     id: 1,
     name: "Classic Leather Bag",
     price: 2500,
-    image: "images/bag1.jpg"
+    image: "https://images.unsplash.com/photo-1590874103328-eac38a683ce7"
   },
   {
     id: 2,
-    name: "Luxury Handbag",
+    name: "Luxury Designer Bag",
     price: 3200,
-    image: "images/bag2.jpg"
-  },
-  {
-    id: 3,
-    name: "Travel Backpack",
-    price: 4000,
-    image: "images/bag3.jpg"
-  },
-  {
-    id: 4,
-    name: "Casual Shoulder Bag",
-    price: 1800,
-    image: "images/bag4.jpg"
+    image: "https://images.unsplash.com/photo-1584917865442-de89df76afd3"
   }
 ];
