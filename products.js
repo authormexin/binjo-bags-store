ea24e41126a04f17d0376b04f1fc40f2d39be824
authormexin <authormexin@gmail.com>
2026-04-16 09:33:07 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: 1,
+    name: "Classic Leather Bag",
+    price: 2500,
+    image: "images/bag1.jpg"
+  },
+  {
+    id: 2,
+    name: "Luxury Handbag",
+    price: 3200,
+    image: "images/bag2.jpg"
+  },
+  {
+    id: 3,
+    name: "Travel Backpack",
+    price: 4000,
+    image: "images/bag3.jpg"
+  },
+  {
+    id: 4,
+    name: "Casual Shoulder Bag",
+    price: 1800,
+    image: "images/bag4.jpg"
+  }
+];
